438bd495ee5c9bcec6c916d0a7dbd0720de5d00c
ian <38205068+im-ian@users.noreply.github.com>
2023-12-06 12:24:17 +0900

    Chore: remove import

--- src/services/user.ts
+++ src/services/user.ts
@@ -1,6 +1,5 @@
 import { fetcher } from "./fetch";
 import { User } from "../types/user";
-import { wrapPromise } from "../utils/promise";
 
 export function getUserList() {
   const result = fetcher<User[]>("https://jsonplaceholder.typicode.com/users");
